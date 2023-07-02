fbc25d36e36bc22fae8a82eb3216e8171648bbdc
Eugene Park <eugenepark404@gmail.com>
2023-07-01 18:49:35 -0700

    Changed more project stuff about cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,6 +5,11 @@ import { motion } from 'framer-motion';
 import { Card, Grid, Text, Button, Row, Modal, useModal } from "@nextui-org/react";
 import Image from 'next/image';
 import { useState } from 'react';
+import nextIcon from './icons/next-js.svg';
+import vercelIcon from './icons/vercel-icon-svgrepo-com.svg';
+import wordpressIcon from './icons/wordpress-color-svgrepo-com.svg';
+import pleskIcon from './icons/plesk-svgrepo-com.svg';
+import yoastIcon from './icons/yoast-svgrepo-com.svg';
 
 function Projects() {
     //products list
@@ -13,15 +18,40 @@ function Projects() {
             id: 1,
             title: "Retro Audio",
             img: "pw/AJFCJaUXIWcyDrMLMfK1BAziYYNDF93Dqb2EqaMQWX-Sesx_6iGws75mRi_bcAYLc7lwOzKBlrdCvWRTgosLQitAjKea23s_WA8xiYw4AaeCy0EU565aPQ=w2400",
-            description: "$120",
-            itemInfo: "retroAudio",
+            description: "Retro Audio is my first project. It is a website for a fictional company that sells retro audio equipment.It was built using React, Next.js, and Tailwind CSS. It is hosted on Vercel.",
+            icons: [{
+                id: 1,
+                title: "Next.js",
+                url: "https://nextjs.org/",
+                icon: <Image src={nextIcon} alt="Next.js" width={30} height={30} />
+            }, {
+                id: 2,
+                title: "Vercel",
+                url: "https://vercel.com/",
+                icon : <Image src={vercelIcon} alt="Vercel" width={30} height={30} />
+            }]
         },
         {
             id: 2,
             title: "Rancho Drive Train",
             img: "pw/AJFCJaV2ZCSjeSFMUeJiSJftrM0u_DAQWhIuzwFcpYcDowPs9PtxnWNnKIKLH5g2kHtzlkVYrCbeLpTzlg1UeQcbTGyW4a141qw-ud3bSkV1_dUoTgroRQ=w2400",
             description: "$120",
-            itemInfo: "RDE",
+            icons: [{
+                id: 1,
+                title: "Wordpress",
+                url: "https://wordpress.com/",
+                icon: <Image src={wordpressIcon} alt="Next.js" width={30} height={30} />
+            }, {
+                id: 2,
+                title: "Plesk",
+                url: "https://www.plesk.com/",
+                icon : <Image src={pleskIcon} alt="Vercel" width={30} height={30} />
+            }, {
+                id: 3,
+                title: "Yoast",
+                url: "https://yoast.com/",
+                icon : <Image src={yoastIcon} alt="Vercel" width={30} height={30} />
+            }]
         }
       ];
 
@@ -50,50 +80,67 @@ function Projects() {
                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 1, transition: { duration: .5 }}}
         >
-        <Grid.Container gap={2} justify="center">
-        {list.map((item, index) => (
-            <Grid className={styles.cardGrid} key={index}>
-                <Card isPressable isHoverable onPress={() => handleCardPress(item.id)}>
-                    <Card.Body css={{p: 0}}>
-                    <Card.Image
-                        src={"https://lh3.googleusercontent.com/" + item.img}
-                        referrerPolicy='no-referrer'
-                        objectFit="scale-down"
-                        width="100%"
-                        height="100%"
-                        alt={item.title}
-                    />
-                    </Card.Body>
-                    <Card.Footer css={{ justifyItems: "flex-start" }}>
-                    <Row wrap="wrap" justify="space-between" align="center">
-                        <Text b className={styles.itemTitle}>{item.title}</Text>
-                        <Text className={styles.itemPrice}>
-                        {item.description}
-                        </Text>
-                    </Row>
-                    </Card.Footer>
-                </Card>
-            </Grid>
-        ))}
+        <Grid.Container gap={0} justify="space-around" css={{p: 50}} >
+            {list.map((item, index) => (
+                <Grid className={styles.cardGrid} key={index}>
+                    <Card isPressable isHoverable onPress={() => handleCardPress(item.id)}>
+                        <Card.Image
+                            css={{p : 0}}
+                            src={"https://lh3.googleusercontent.com/" + item.img}
+                            referrerPolicy='no-referrer'
+                            objectFit="scale-down"
+                            width="100%"
+                            height="100%"
+                            alt={item.title}
+                        />
+                        <Card.Footer isBlurred css={{ justifyItems: "flex-start" }}>
+                        <Row wrap="wrap" justify="space-between" align="center">
+                            <Text b className={styles.itemTitle}>{item.title}</Text>
+                            <div className={styles.iconList}>
+                                {item.icons.map((icon, index) => (
+                                    <div key={index} className={styles.iconItem}>
+                                        <a href={icon.url} target="_blank">{icon.icon}</a>
+                                    </div>
+                                ))}
+                            </div>
+                        </Row>
+                        </Card.Footer>
+                    </Card>
+                </Grid>
+            ))}
         </Grid.Container>
         </motion.div>
         <Modal
+            noPadding
             scroll
             width="900px"
             open={isModalOpen} onClose={() => setIsModalOpen(false)}
+            className={styles.modal}
         >
-            <Modal.Header>
-            <Text id={selectedItemId?.toString()} className={styles.modalHeader}>
-                {list.find(item => item.id === selectedItemId)?.title}
-            </Text>
+            <Modal.Header className={styles.modalHeader}>
+            <Image 
+            className={styles.modalImage}
+            width={900}
+            height={500}
+            alt={list.find(item => item.id === selectedItemId)?.title ?? "Modal Image"} 
+            src={"https://lh3.googleusercontent.com/" + list.find(item => item.id === selectedItemId)?.img} 
+            referrerPolicy='no-referrer'
+            />
+            <Row wrap="wrap" justify="space-evenly" align="center" css={{p: 10}}>
+                <Text b className={styles.itemTitle}>{list.find(item => item.id === selectedItemId)?.title}</Text>
+                <div className={styles.iconList}>
+                    {list.find(item => item.id === selectedItemId)?.icons.map((icon, index) => (
+                        <div key={index} className={styles.iconItem}>
+                            <a href={icon.url} target="_blank">{icon.icon}</a>
+                        </div>
+                    ))}
+                </div>
+            </Row>
             </Modal.Header>
             <Modal.Body>
-            <Text id={selectedItemId?.toString()} className={styles.modalText}>
+            <Text className={styles.itemPrice}>
                 {list.find(item => item.id === selectedItemId)?.description}
-            </Text>
-            <Text id={selectedItemId?.toString()} className={styles.modalText}>
-                {list.find(item => item.id === selectedItemId)?.itemInfo}
-            </Text>
+                </Text>
             </Modal.Body>
             <Modal.Footer>
             <Button auto flat color="error" onPress={() => setIsModalOpen(false)}>
