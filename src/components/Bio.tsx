c6bdd4b6c8f056b870e5deaf6cf9704ca622dd73
Eugene Park <eugenepark404@gmail.com>
2023-07-07 14:48:26 -0700

    Resume link works

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import { Button } from '@nextui-org/react';
 
 function Bio() {
+
   return (
     <div id="about" className='page'>
       <motion.div
@@ -17,9 +18,11 @@ function Bio() {
             >
               <p className="paragraph">{"I am currently a Computer Science student at Chaffey College and I also study software development in my free time. My passion in development has led me to expand my skill set and problem-solve in more creative ways!As a growing developer, I\'m constantly learning new programming languages and exploring various web development frameworks. I thrive on challenges and am always seeking opportunities to push the boundaries of what I can do. My creative mind and strong work ethic set me apart, and I\'m excited to make a meaningful impact in the tech industry in the future!"}</p>
               <div className='button'>
+                <a href="./FrontEndDeveloper.pdf" target="_blank">
                 <Button bordered size="lg" color="primary" rounded ghost>
                   Resume
                 </Button>
+                </a>
               </div>
       </motion.div>
     </div>
